80189c1204e66037061ea70f0b46aba3b60f5eba
Seamile <lanhuermao@gmail.com>
2018-10-07 01:20:45 +0800

    add chat system

--- web/tantan/settings.py
+++ web/tantan/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
     'common.middleware.JsonMiddleware',
     'common.middleware.AuthMiddleware',
 ]
@@ -104,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
